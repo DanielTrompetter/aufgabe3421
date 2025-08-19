0ec644d66a38417222da319ffc97e93a0d00e0ca
Daniel Trompetter <danieltrompetter@gmail.com>
2025-08-19 13:23:31 +0200

    init

---
+++ bin/aufgabe3421.dart
@@ -0,0 +1,5 @@
+import 'package:aufgabe3421/aufgabe3421.dart' as aufgabe3421;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aufgabe3421.calculate()}!');
+}
---
+++ test/aufgabe3421_test.dart
@@ -0,0 +1,8 @@
+import 'package:aufgabe3421/aufgabe3421.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
