93291294e62dacc09d967c4141960b671953b6f5
Daniel Trompetter <danieltrompetter@gmail.com>
2025-08-19 13:42:02 +0200

    Done

--- bin/aufgabe3421.dart
+++ bin/aufgabe3421.dart
@@ -1,5 +1,25 @@
-import 'package:aufgabe3421/aufgabe3421.dart' as aufgabe3421;
+void main() 
+{
+   int  age = 13;                    // (Alter der Person)
+  bool hasParentalConsent = false;   //(Erlaubnis der Eltern vorhanden?)
+  int movieAgeRating = 16;          // (Altersfreigabe des Films)
 
-void main(List<String> arguments) {
-  print('Hello world: ${aufgabe3421.calculate()}!');
+  if(age>movieAgeRating ||(hasParentalConsent && age>movieAgeRating-2))
+  {
+    print("Go to Cinema!");
+  }
+  else
+  {
+    print("Stay at home!");
+  }
+
+
+/*
+Schreibe eine if-Bedingung mit logischen Operatoren, die prüft ob:
+die Person alt genug für den Film ist ODER
+die Person die Erlaubnis der Eltern hat UND nicht mehr als 2 Jahre unter der Altersfreigabe liegt
+Teste deine Lösung mit diesen Werten:
+Test 1: age = 15, hasParentalConsent = true, movieAgeRating = 16
+Test 2: age = 13, hasParentalConsent = false, movieAgeRating = 16
+*/
 }
--- test/aufgabe3421_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:aufgabe3421/aufgabe3421.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
